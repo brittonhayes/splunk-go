3f624cfbe4cc73b6cb2482f32765072cd43d2aec
efernandezbrei <eferandezbrei@hotels.com>
2019-09-09 12:47:34 +0100

    added outputmode parameters for searches

--- searches.go
+++ searches.go
@@ -17,3 +17,16 @@ func (conn SplunkConnection) SearchSync(searchString string) (string, error) {
 	response, err := conn.httpPost(fmt.Sprintf("%s/services/search/jobs/export", conn.BaseURL), &data)
 	return response, err
 }
+
+func (conn SplunkConnection) SearchSyncWithOutputMode(searchString string, outputMode string) (string, error) {
+	data := make(url.Values)
+	data.Add("search", searchString)
+	data.Add("max_count", "1")
+	data.Add("max_time", "60")
+	data.Add("earliest_time", "-10m")
+	data.Add("latest_time", "now")
+	data.Add("output_mode", outputMode)
+
+	response, err := conn.httpPost(fmt.Sprintf("%s/services/search/jobs/export", conn.BaseURL), &data)
+	return response, err
+}
